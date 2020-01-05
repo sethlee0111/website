269d4a1c12f83cfb5ce34cc387b4fc4140ddad40
Seth <sethlee@utexas.edu>
2020-01-05 09:53:01 +0900

    .

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = angular.module("seth_website", ['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+      templateUrl : 'pages/home.html',
+      controller  : 'HomeController'
+    })
+
+    .when('/my_works', {
+      templateUrl : 'pages/my_works.html',
+      controller  : 'MyWorksController'
+    })
+
+    .when('/resources', {
+      templateUrl : 'pages/resources.html',
+      controller  : 'ResourcesController'
+    })
+
+    .otherwise({redirectTo: '/'});
+    
+    });
+
+app.controller('HomeController', function($scope) {
+  $scope.message = 'Hello from HomeController';
+});
+
+app.controller('MyWorksController', function($scope) {
+  $scope.message = 'Hello from MyWorksController';
+});
+
+app.controller('ResourcesController', function($scope) {
+  $scope.message = 'Hello from ResourcesController';
+});
